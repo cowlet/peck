5dc1517676c9d63325540bd1a5555fa359e6ee5f
Victoria Catterson <vcatterson@gmail.com>
2012-08-20 09:10:02 +0100

    First pages commit

---
+++ peck.js
@@ -0,0 +1,433 @@
+var PECK = {};
+
+// *** Yard ***
+// The yard is the object that holds all the chickens, grain, etc
+PECK.yard = {
+  width: 620,
+  height: 300,
+  mouse_state: undefined,
+  chickens: [],
+  grains: [],
+  
+  set_mouse: function (ms) {
+    this.mouse_state = ms;
+  },
+
+  unique_name: function (n) {
+    // If we get a collision, increase the range of name search
+    for (var i = 10; ; i+= 10)
+    {
+      var potential = n + "-" + PECK.rand(i);
+      //console.log("Potential name is " + potential);
+      if (this.chickens.every (function (c) { return (c.name !== potential) }))
+      {
+        //console.log ("Returning " + potential);
+        return potential;
+      }
+    }
+  },
+  
+  add_chicken: function (c) {
+    this.chickens.push (c);
+  },
+  
+  add_grain: function (g) {
+    this.grains.push (g);
+    
+    // Provoke the chickens
+    this.chickens.forEach (function (c) { c.start_chase({ "x": g.x, "y": g.y-c.height }); });
+  },
+  
+  is_grain_around: function (c) {
+    // can chicken c eat grain
+    var that = this;
+    this.grains.some (function (g) {      
+      // can chicken see grain
+      if (c.can_see (g) || c.standing_near (g))
+      {
+        c.start_chase ({ "x": g.x, "y": g.y-c.height });
+      };
+      
+      // can chicken eat grain
+      if (PECK.approx_equals (c.x, g.x, 5) &&
+          PECK.approx_equals (c.y+c.height, g.y, 5))
+      {
+        //console.log ("Chicken " + c.name + " is eating grain " + that.grains.indexOf(g));
+        c.stop_chase (true);
+        that.grains.splice (that.grains.indexOf (g), 1); // delete grain
+        // eating is highest priority: don't look for more grain
+        return true;
+      };
+      return false;
+    });
+    
+    // if there is no more grain, stop the chase
+    if (this.grains.length === 0)
+    {
+      c.stop_chase (false);
+    }
+  },
+  
+  
+  check_for_mouse_on_chickens: function () {
+    if (this.mouse_state === undefined)
+    {
+      return;
+    }
+
+    var that = this;
+    this.chickens.forEach (function (c) { 
+      if (PECK.approx_equals (c.x, that.mouse_state.x, 25) &&
+          PECK.approx_equals (c.y+c.height, that.mouse_state.y, 25))
+      {
+        PECK.GFX.write_chicken_name (c.name, c.x, c.y+22);
+      }
+    });    
+  }
+};
+
+// *** Infobar ***
+// The infobar prints all the chicken info
+PECK.infobar = {  
+  day: 0,
+  hour: 6,
+  money: 10,
+  
+  increment_time: function () {
+    this.hour += 1;
+    if (this.hour > 23)
+    {
+      this.hour = 0;
+      this.day += 1;
+    }
+  },
+  
+  day_or_night: function () {
+    if (this.hour > 7 && this.hour < 19)
+    {
+      return "day";
+    }
+    else if (this.hour === 7 || this.hour === 19)
+    {
+      return "between";
+    }
+    else
+    {
+      return "night";
+    }
+  }
+}
+
+
+PECK.egg_creator = function (n, startx, starty) {
+  return {
+    name: n,
+    x: startx, 
+    y: starty, 
+    height: 20,
+    satiation: 100,
+    happiness: 100,
+    health: 5,
+    behaviour: { move: "egg" },
+    time_to_update: 0,
+    birthday: PECK.infobar.day,
+    birthhour: PECK.infobar.hour,
+
+    sells_for: function () {
+      return Math.floor(this.happiness/10);
+    },
+
+    // An egg isn't interested in grain!
+    start_chase: function () {
+    },
+    stop_chase: function () {
+    },
+
+    hours_since_birth: function () {
+      return ((PECK.infobar.day - this.birthday) * 24 +
+              (PECK.infobar.hour - this.birthhour));
+    },
+
+    update_tasks: function () {
+      // All an egg has to do is check if it's time to hatch
+      //console.log ("Age is " + this.hours_since_birth () + "hours");
+      if (this.hours_since_birth () >= 3)
+      {
+        //console.log ("Hatching...");
+        this.hatch ();
+      }
+    },
+
+    update: function () {
+      this.time_to_update -= 1;
+      if (this.time_to_update > 0)
+      {
+        return;
+      }
+
+      this.update_tasks ();
+      this.time_to_update = PECK.timeout()/10 + PECK.rand( PECK.timeout()/10 );
+    },
+
+    hatch: function () {
+      // Add all the chicken data
+      this.height = 20;
+      this.heading = PECK.rand (360);
+      this.frame = 0;
+      this.chasing = false;
+      this.last_lay_day = PECK.infobar.day; // don't lay first day
+      
+      // Swap to the more complex chicken behaviour
+      this.behaviour = {
+        moves: ["standing", "bok", "peck", "walk", "scratch", "chase"],
+        transitions: {
+          "standing": [10, 20, 10, 40, 20,   0],
+          "bok":      [50, 50,  0,  0,  0,   0],
+          "peck":     [20,  0, 50,  0, 30,   0],
+          "walk":     [30,  0,  0, 70,  0,   0],
+          "scratch":  [20,  0, 20,  0, 60,   0],
+          "chase":    [ 0,  0,  0,  0,  0, 100]
+        },
+        move: "standing",
+
+        next_move: function () {
+          var p = PECK.rand(100);
+          var that = this;
+          // For the correct row of transitions, sum elements one by
+          // one until we find the partial_sum greater or equal to p    
+          this.transitions[this.move].some (function (value, i) {
+            this.partial_sum += value;
+            if (p <= this.partial_sum)
+            {
+              //console.log ("Move for p = " + p + " should be to " + that.moves[i]);
+              that.move = that.moves[i];
+              return true;
+            }
+            return false;
+          }, {partial_sum: 0});
+        }
+      };
+    
+      // Now add all the chicken functions
+      this.direction = function () {
+        return (this.heading < 180 ? "east" : "west");
+      };
+    
+      this.rad_heading = function () {
+        return this.heading * Math.PI / 180;
+      };
+      
+      this.start_chase = function (pos) {
+        this.chasing = pos;
+        this.behaviour.move = "chase";
+      };
+      
+      this.stop_chase = function (caught) {
+        this.chasing = false;
+        if (caught)
+        {
+          this.satiation = Math.min (100, this.satiation+10);
+          this.behaviour.move = "peck";
+        }
+        else if (this.behaviour.move === "chase")
+        {
+          this.behaviour.move = "standing";
+        };
+        // else, it wasn't chasing, so keep moving as before
+      };  
+
+      this.can_see = function (grain) {
+        if (this.direction() === "west" && (grain.x < this.x) ||
+            this.direction() === "east" && (grain.x > this.x))
+        {
+          if (PECK.approx_equals (this.x, grain.x, 25) &&
+              PECK.approx_equals (this.y+this.height, grain.y, 25))
+          {
+            //console.log (this.name + " can see grain");
+            return true;
+          }
+        }
+        return false;
+      };
+    
+      this.standing_near = function (grain) {
+        if (PECK.approx_equals (this.x, grain.x, 5) &&
+            PECK.approx_equals (this.y+this.height, grain.y, 5))
+        {
+          //console.log (this.name + " is near grain");
+          return true;
+        }
+        return false;
+      };
+      
+      this.update_heading = function () {          
+        if (this.chasing)
+        {
+          // at x,y going to chasing.x,chasing.y
+          var h = PECK.rand(90);
+          // generate a new heading into the quadrant of the chase
+          if (this.chasing.x > this.x && this.chasing.y < this.y)
+            this.heading = h;
+          else if (this.chasing.x > this.x && this.chasing.y > this.y)
+            this.heading = 90+h;
+          else if (this.chasing.x < this.x && this.chasing.y > this.y)
+            this.heading = 180+h;
+          else
+            this.heading = 270+h;
+        }
+        else
+        {
+          // If we're not chasing, 1 in 5 times change direction
+          if (!PECK.rand(5)) 
+            this.heading = PECK.rand (360);
+        }
+      };
+    
+      this.update_position = function () {    
+        if (this.behaviour.move === "walk" ||
+            this.behaviour.move === "chase")
+        {
+          // chickens move 10 pix
+          this.x += 10 * Math.sin(this.rad_heading());
+          this.y -= 10 * Math.cos(this.rad_heading());
+        }
+      };
+
+      this.chance_of_laying = function () {
+        if ((this.last_lay_day < PECK.infobar.day) &&
+            (PECK.infobar.hour === 6) &&
+            (PECK.rand (this.happiness) > 10))
+        {
+          var name = prompt("An egg is laid!\nName your egg:", PECK.yard.unique_name (this.name));
+          PECK.yard.add_chicken (PECK.egg_creator (name, this.x, this.y+this.height));
+          this.last_lay_day = PECK.infobar.day;
+        }
+      };
+
+      this.update_happiness = function () {
+        var addition = (this.satiation - 50) / 5;
+        this.happiness = Math.max (0, Math.min (100, this.happiness+addition));
+      };
+
+      this.health_check = function () {
+        if (this.satiation === 0)
+        {
+          this.health -= 1;
+        }
+        if (this.happiness === 0)
+        {
+          this.health -= 1;
+        }
+
+        if (this.health <= 0)
+        {
+          /* Farewall, c.name, we hardly knew ye */
+          this.behaviour = { move: "dead" };
+          this.happiness = 0;
+          this.satiation = 0;
+          this.update_tasks = function () { };
+
+          // Prevent zombie chickens from chasing grain
+          this.start_chase = function () { };
+          this.stop_chase = function () { };
+        }
+      };
+
+      // Finally, rewrite the update tasks for chicken behaviour
+      this.update_tasks = function () {
+        //console.log ("Chicken is updating");
+        this.behaviour.next_move ();
+        this.chance_of_laying ();
+        PECK.yard.is_grain_around (this);
+        this.update_heading ();
+        this.update_position ();
+        this.frame ? this.frame = 0 : this.frame = 1;
+        
+        this.satiation = Math.max (0, this.satiation-1);
+        this.update_happiness ();
+        this.health_check ();
+      };
+ 
+    }
+  };
+};
+
+PECK.ending = function () {
+  if (PECK.infobar.money <= 0)
+  {
+    return ("Game over!\nYou ran out of money :(");
+  }
+  else if (PECK.yard.chickens.length === 0)
+  {
+    return ("Game over!\nYou closed down the farm.\nYou made $" + PECK.infobar.money);
+  }
+  else if (PECK.yard.chickens.every (function (c) { return c.behaviour.move === "dead"; }))
+  {
+    return ("Game over!\nAll your chickens are dead!\nYou meanie!");
+  }
+  return null;
+};
+
+PECK.timeout = function () {
+  if (document.getElementById ("fast").checked)
+  {
+    return 30;
+  }
+  else if (document.getElementById ("medium").checked)
+  {
+    return 150;
+  }
+  
+  return 300; // default is slow
+};
+
+
+// *** Game loop ***
+// game_loop prints and updates the full yard of chickens
+PECK.game_loop = function (counter) {
+  // handle yard updates
+  PECK.yard.chickens.forEach (function (c) { c.update (); });
+  PECK.yard.draw();
+  
+  // handle time updates
+  counter += 1;
+  if (counter > PECK.timeout ())
+  {
+    PECK.infobar.increment_time();
+    counter = 0;
+  }
+  
+  // check for end states
+  var end_msg = PECK.ending ();
+  if (end_msg)
+  {
+    alert(end_msg);
+    return;
+  }
+      
+  var t = setTimeout(PECK.game_loop, 10, counter);
+};
+
+
+// *** Starting point ***
+PECK.setup = function () {  
+
+  var canvas = document.getElementById("canvas");
+  PECK.yard.width = canvas.width;
+  PECK.yard.height = canvas.height;
+  PECK.GFX.set_ctx (canvas.getContext("2d"));
+
+  canvas.addEventListener("click", PECK.GFX.handleClick, false);
+  canvas.addEventListener("mousemove", function (e) {
+    PECK.yard.set_mouse(PECK.getCursorPosition(e));
+  }, false);
+  
+  ["Henrietta", "Henelope", "Henderson"].forEach (function (n) {
+    PECK.yard.add_chicken (PECK.egg_creator (n,
+                                             PECK.rand (PECK.yard.width), 
+                                             PECK.rand (PECK.yard.height)));
+  });
+
+  PECK.game_loop (0);
+};  
+
---
+++ peck_graphics.js
@@ -0,0 +1,482 @@
+PECK.GFX =  {};
+
+PECK.GFX.colourways = {
+  day: {
+    ground: "rgb(249,238,137)",
+    body: "brown",
+    beaksnfeet: "orange",
+    grain: "brown",
+    text: "black",
+    egg: "white",
+    grave: "rgb(100,100,100)",
+  },
+  night: {
+    ground: "rgb(50,50,100)",
+    body: "rgb(20,20,20)",
+    beaksnfeet: "rgb(165,150,150)",
+    grain: "black",
+    text: "white",
+    egg: "rgb(240,240,220)",
+    grave: "black",
+  },
+  between: {
+    ground: "rgb(242,159,153)",
+    body: "brown",
+    beaksnfeet: "rgb(220,120,90)",
+    grain: "brown",
+    text: "white",
+    egg: "rgb(240, 240, 220)",
+    grave: "rgb(80,80,80)",
+  },
+};
+
+PECK.GFX.colouring = function (area) {
+  return PECK.GFX.colourways[PECK.infobar.day_or_night()][area];
+};
+
+PECK.GFX.set_ctx = function (ctx) {
+  PECK.GFX.ctx = ctx;
+};
+
+
+/*** Yard updates ***/
+PECK.yard.draw = function () {
+  var i;
+  
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring ("ground");
+  PECK.GFX.ctx.fillRect(0, 0, this.width, this.height);
+
+  this.grains.forEach (function (g) { PECK.GFX.draw_grain (g); });
+  this.chickens.forEach (function (c) { PECK.GFX.select_chicken_frame (c); });
+  
+  PECK.infobar.draw (this.chickens);
+  
+  this.check_for_mouse_on_chickens ();
+};
+
+
+/*** Infobar updates ***/
+PECK.infobar.x = 0;
+PECK.infobar.y = PECK.yard.height;
+PECK.infobar.width = PECK.yard.width;
+PECK.infobar.height = 100;
+PECK.infobar.line_height = 15;
+PECK.infobar.y_indent = 20;
+PECK.infobar.x_columns = [15, 100, 200, 360];
+
+PECK.infobar.draw = function (chickens) {
+  chickens.forEach (function (c) {
+    PECK.infobar.set_chicken_row (c);
+    PECK.infobar.draw_bar (c.name + "-sat-bar", c.satiation);
+    PECK.infobar.draw_bar (c.name + "-hap-bar", c.happiness);
+  });
+
+  var farm_html = "<p>Day: " + this.day + " Time: " + this.hour + ":00</p><p>Money: $" +
+                  this.money + "</p>";
+  document.getElementById("farm-info-text").innerHTML = farm_html;
+};
+
+PECK.infobar.set_chicken_row = function (c) {
+  var row = document.getElementById (c.name + "-row");
+  if (row === null)
+  {
+    row = document.getElementById ("chicken-info-body").insertRow(-1);
+    row.setAttribute ("id", c.name+"-row");
+
+    var cell = row.insertCell (0);
+    cell.setAttribute ("id", c.name+"-name");
+    cell.setAttribute ("class", "chicken-name");
+    cell.innerText = c.name;
+    
+    cell = row.insertCell (1);
+    cell.setAttribute ("id", c.name+"-act");
+    cell.innerText = c.behaviour.move;
+    
+    cell = row.insertCell (2);
+    cell.setAttribute ("id", c.name+"-sat");
+    cell.innerHTML = "<canvas class=\"bar\" id=\"" + c.name + "-sat-bar\"></canvas>";
+    
+    cell = row.insertCell (3);
+    cell.setAttribute ("id", c.name+"-hap");
+    cell.innerHTML = "<canvas class=\"bar\" id=\"" + c.name + "-hap-bar\"></canvas>";
+    
+    cell = row.insertCell (4);
+    cell.setAttribute ("id", c.name+"-sell");
+    cell.setAttribute ("class", "sell-text");
+    cell.innerHTML = "<a href=\"#\" id=\"sell-" + c.name + "\" class=\"sell-link\" onclick=\"PECK.sell_direct('" + c.name+ "');\">+$" + c.sells_for () + "</a>";
+  }
+  else
+  {
+    document.getElementById (c.name+"-act").innerText = c.behaviour.move;
+    if (c.sells_for () > 0)
+    {
+      document.getElementById ("sell-" + c.name).innerText = "+$" + c.sells_for ();
+    }
+    else if (document.getElementById("sell-" + c.name) !== null)
+    {
+      /* The cell should no longer contain an <a> to sell the chicken */
+      var cell = document.getElementById(c.name + "-sell");
+      cell.innerText = "+$0";
+      cell.removeAttribute ("class");
+    }
+    // else, sells for 0 and link is already removed
+  }
+};
+
+PECK.infobar.draw_bar = function (element_name, value) {
+  var canvas = document.getElementById (element_name);
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "black";
+  ctx.fillRect (0, 0, canvas.width, canvas.height);
+  var xmarg = 5;
+  var ymarg = 10;
+  
+  var graph_length = (canvas.width - 2*xmarg) * value/100;
+  if (value > 60)
+  {
+      ctx.fillStyle = "green";
+  }
+  else if (value > 25)
+  {
+    ctx.fillStyle = "yellow";
+  }
+  else
+  {
+    ctx.fillStyle = "red";
+  }
+  ctx.fillRect (xmarg, ymarg, graph_length+1, canvas.height-2*ymarg);
+  ctx.fillStyle = "white";
+  ctx.fillRect ((xmarg+graph_length), ymarg,
+                (canvas.width - graph_length - 2*xmarg), canvas.height-2*ymarg);
+};
+
+
+/*** Chicken drawing functions ***/
+PECK.GFX.select_chicken_frame = function (c) {
+  //PECK.GFX.draw_hitbox (c);
+
+  if (c.behaviour.move === "egg")
+  {
+    PECK.GFX.draw_egg (c);
+    return;
+  }
+  if (c.behaviour.move === "dead")
+  {
+    PECK.GFX.draw_grave (c);
+    return;
+  }
+  
+  //console.log ("Selecting frame for " + c.behaviour.move + ", " + c.frame);
+  if (c.frame === 0)
+  {
+    if (c.behaviour.move === "standing")
+      PECK.GFX.draw_facing(c);
+    else if (c.behaviour.move === "peck")
+      PECK.GFX.draw_pecking(c);
+    else if (c.behaviour.move === "bok")
+      PECK.GFX.draw_bokking(c);
+    else if (c.behaviour.move === "walk" || c.behaviour.move === "chase")
+      PECK.GFX.draw_walking(c);
+    else if (c.behaviour.move === "scratch")
+      PECK.GFX.draw_scratching(c);
+  }
+  else
+  {
+    if (c.behaviour.move === "scratch")
+      PECK.GFX.draw_scratching(c);
+    else
+      PECK.GFX.draw_facing(c);
+      // standing, peck, walk, bok all have the same off frame
+  }
+};
+
+PECK.GFX.draw_facing = function (c) {
+  var bodyX = c.x;
+  var bodyY = c.y;
+  // multiplier is pos or neg depending on direction of facing
+  // affects x offsets only
+  var m = (c.direction() === "west" ? 1 : -1);
+
+  // beak
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX + (-4*m), bodyY+4);
+  PECK.GFX.ctx.lineTo(bodyX, bodyY+8);
+  PECK.GFX.ctx.fill();
+
+  // body    
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("body");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX, bodyY+16);
+  PECK.GFX.ctx.lineTo(bodyX + (8*m), bodyY+16);
+  PECK.GFX.ctx.fill();
+
+  // feet
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX + (4*m), bodyY+16);
+  PECK.GFX.ctx.lineTo(bodyX + (2*m), bodyY+20);
+  PECK.GFX.ctx.lineTo(bodyX + (6*m), bodyY+20);
+  PECK.GFX.ctx.fill();
+};
+
+
+PECK.GFX.draw_bokking = function (c) {
+  PECK.GFX.draw_facing (c);
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("text");
+    PECK.GFX.ctx.font = "9px sans-serif";
+    PECK.GFX.ctx.textBaseline = "top";
+
+    var xpos = (c.direction() === "west" ? c.x-24 : c.x+6);
+    PECK.GFX.ctx.fillText("bok!", xpos, c.y);
+};
+
+PECK.GFX.draw_pecking = function (c) {
+  var m = (c.direction() === "west" ? 1 : -1);
+  var bodyX = c.x - (12*m);
+  var bodyY = c.y+11;
+
+  // body
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("body");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX + (12*m), bodyY+5);
+  PECK.GFX.ctx.lineTo(bodyX + (18*m), bodyY);
+  PECK.GFX.ctx.fill();
+
+  // beak
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX + (3*m), bodyY+8);
+  PECK.GFX.ctx.lineTo(bodyX + (6*m), bodyY+3);
+  PECK.GFX.ctx.fill();
+
+  // feet
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX + (15*m), c.y+16);
+  PECK.GFX.ctx.lineTo(bodyX + (13*m), c.y+20);
+  PECK.GFX.ctx.lineTo(bodyX + (17*m), c.y+20);
+  PECK.GFX.ctx.fill();
+};
+
+PECK.GFX.draw_walking = function (c) {
+  var bodyX = c.x;
+  var bodyY = c.y;
+  var m = (c.direction() === "west" ? 1 : -1);
+
+  // beak
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX + (-4*m), bodyY+4);
+  PECK.GFX.ctx.lineTo(bodyX, bodyY+8);
+  PECK.GFX.ctx.fill();
+
+  // body    
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("body");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX, bodyY+16);
+  PECK.GFX.ctx.lineTo(bodyX + (8*m), bodyY+16);
+  PECK.GFX.ctx.fill();
+
+  // feet
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");;
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX + (2*m), bodyY+16);
+  PECK.GFX.ctx.lineTo(bodyX + (1*m), bodyY+20);
+  PECK.GFX.ctx.lineTo(bodyX + (3*m), bodyY+20);
+  PECK.GFX.ctx.fill();
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX + (5*m), bodyY+16);
+  PECK.GFX.ctx.lineTo(bodyX + (4*m), bodyY+20);
+  PECK.GFX.ctx.lineTo(bodyX + (6*m), bodyY+20);
+  PECK.GFX.ctx.fill();
+};
+
+
+PECK.GFX.draw_scratching = function (c) {
+  var m = (c.direction() === "west" ? 1 : -1);
+  var bodyX = c.x - (8*m);
+  var bodyY = c.y+8;
+
+  // body
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("body");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX + (10*m), bodyY+8);
+  PECK.GFX.ctx.lineTo(bodyX + (18*m), bodyY+5);
+  PECK.GFX.ctx.fill();
+
+  // beak
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(bodyX, bodyY);
+  PECK.GFX.ctx.lineTo(bodyX + (3*m), bodyY+8);
+  PECK.GFX.ctx.lineTo(bodyX + (6*m), bodyY+3);
+  PECK.GFX.ctx.fill();
+
+  // feet
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("beaksnfeet");
+  PECK.GFX.ctx.beginPath();
+  if (c.frame === 0)
+  {
+    PECK.GFX.ctx.moveTo(bodyX + (13*m), c.y+16);
+    PECK.GFX.ctx.lineTo(bodyX + (11*m), c.y+20);
+    PECK.GFX.ctx.lineTo(bodyX + (15*m), c.y+20);
+    PECK.GFX.ctx.fill();
+  }
+  else
+  {
+    PECK.GFX.ctx.moveTo(bodyX + (13*m), c.y+16);
+    PECK.GFX.ctx.lineTo(bodyX + (11*m), c.y+20);
+    PECK.GFX.ctx.lineTo(bodyX + (13*m), c.y+20);
+    PECK.GFX.ctx.fill();
+    PECK.GFX.ctx.beginPath();
+    PECK.GFX.ctx.moveTo(bodyX + (13*m), c.y+16);
+    PECK.GFX.ctx.lineTo(bodyX + (17*m), c.y+18);
+    PECK.GFX.ctx.lineTo(bodyX + (18*m), c.y+16);
+    PECK.GFX.ctx.fill();
+  }
+};
+
+PECK.GFX.write_chicken_name = function (name, x, y) {
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring("text");
+  PECK.GFX.ctx.font = "9px sans-serif";
+  PECK.GFX.ctx.textBaseline = "top";
+  PECK.GFX.ctx.fillText(name, x, y);
+};
+
+PECK.GFX.draw_grain = function (g) {
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring ("grain");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.moveTo(g.x, g.y);
+  PECK.GFX.ctx.lineTo(g.x+1, g.y);
+  PECK.GFX.ctx.lineTo(g.x+1, g.y+1);
+  PECK.GFX.ctx.lineTo(g.x, g.y+1);
+  PECK.GFX.ctx.fill();
+};
+
+/*** Egg drawing ***/
+PECK.GFX.draw_egg = function (e) {
+  PECK.GFX.ctx.fillStyle = "rgb(100, 100, 60)";
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.arc (e.x, e.y, 10, Math.PI/6, Math.PI*5/6, false);
+  PECK.GFX.ctx.closePath();
+  PECK.GFX.ctx.fill();
+
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring ("egg");
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.arc (e.x, e.y, 9, 0, Math.PI*2, false);
+  PECK.GFX.ctx.closePath();
+  PECK.GFX.ctx.fill();
+
+  PECK.GFX.ctx.beginPath();
+  PECK.GFX.ctx.arc (e.x, e.y-3, 8, 0, Math.PI*2, true);
+  PECK.GFX.ctx.closePath();
+  PECK.GFX.ctx.fill();
+
+};
+
+/*** Grave drawing ***/
+PECK.GFX.draw_grave = function (c) {
+  PECK.GFX.ctx.fillStyle = PECK.GFX.colouring ("grave");
+  PECK.GFX.ctx.beginPath ();
+  PECK.GFX.ctx.arc (c.x, c.y, 10, 0, Math.PI, true);
+  PECK.GFX.ctx.closePath();
+  PECK.GFX.ctx.fill();
+
+  PECK.GFX.ctx.fillRect (c.x-10, c.y-1, 20, 15);
+
+  PECK.GFX.ctx.fillStyle = "white";
+  PECK.GFX.ctx.font = "9px sans-serif";
+  PECK.GFX.ctx.textBaseline = "top";
+  PECK.GFX.ctx.fillText("RIP", c.x-8, c.y-2);
+};
+
+/*** Mouse events ***/
+PECK.GFX.handleClick = function (e) {
+  var position = PECK.getCursorPosition(e);
+  if (position === undefined) {
+    return;
+  }
+
+  if (document.getElementById ("grain").checked)
+  {
+    PECK.GFX.dropGrainOnClick (position);
+  }
+  else if (document.getElementById ("sell").checked)
+  {
+    PECK.GFX.sell_item (position);
+  }
+};
+
+
+PECK.GFX.dropGrainOnClick = function (position) {
+  // Drop n grains in an nxn square centered on position's x and y
+  var n = 10;
+  for (var i = 0; i < n; i += 1)
+  {
+    PECK.yard.add_grain ( {
+      x: position.x - n/2 + PECK.rand (n), // centered on click
+      y: position.y - n/2 + PECK.rand (n), // centered on click
+    });
+  }
+  // Pay for the grain
+  PECK.infobar.money -= 1;
+  PECK.yard.draw();
+};
+
+PECK.GFX.draw_hitbox = function (chicken) {
+  PECK.GFX.ctx.fillStyle = "black";
+  if (chicken.behaviour.move === "egg")
+  {
+    PECK.GFX.ctx.strokeRect (chicken.x-10, chicken.y-12, 20, 24);
+  }
+  else
+  {
+    PECK.GFX.ctx.strokeRect (chicken.x-10, chicken.y, 20, 20);
+  }
+};
+
+PECK.GFX.sell_item = function (position) {
+  // Is the click on something sellable?
+  var clicked = PECK.yard.chickens.filter (function (c) {
+    if ((c.behaviour.move === "egg") &&
+        PECK.approx_equals (c.x, position.x, 10) &&
+        PECK.approx_equals (c.y, position.y, 12))
+    {
+      return true;
+    }
+    else if ((c.behaviour.move !== "dead") &&
+             PECK.approx_equals (c.x, position.x, 10) &&
+             PECK.approx_equals (c.y+10, position.y, 10))
+    {
+      return true;
+    }
+  });
+
+  if (clicked.length > 0)
+  {
+    PECK.remove_chicken (clicked[0]);
+  }
+};
+
+PECK.sell_direct = function (name) {
+  var chickens = PECK.yard.chickens.filter (function (c) { return (c.name === name); });
+  PECK.remove_chicken (chickens[0]);
+};
+
+PECK.remove_chicken = function (c) {
+  PECK.infobar.money += c.sells_for ();
+  PECK.yard.chickens.splice (PECK.yard.chickens.indexOf (c), 1);
+  var tablerow = document.getElementById (c.name + "-row");
+  tablerow.parentNode.removeChild (tablerow);
+};
+
+
+
